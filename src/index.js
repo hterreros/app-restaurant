69c6b1e91b3a0f5b8d32dcc582ac3bf721ac194e
hterreros <harold.terreros.talledo@gmail.com>
2022-07-30 23:05:43 -0500

    pruebahash

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+    <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
